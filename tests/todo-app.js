ba54bbdfa7725dba27096bb7e764aed240c1cc62
Domgaj01 <domgaj01@easv365.dk>
2025-11-06 22:40:38 +0100

    Running test no.9.3

--- tests/todo-app.js
+++ tests/todo-app.js
@@ -3,16 +3,13 @@ import { Selector } from "testcafe";
 fixture('Todo App E2E Tests')
   .page('http://localhost:5173/');
 
-test('App should load and show the main page', async t => {
-  await t.expect(Selector('body').exists).ok();
-});
-
-test('User can see the todo input field', async t => {
+test('User can add a todo', async t => {
   const input = Selector('input[type="text"]');
-  await t.expect(input.exists).ok();
-});
-
-test('User can click the add button', async t => {
   const button = Selector('button').withText('Add');
-  await t.expect(button.exists).ok();
+  const todoList = Selector('.todo-item');
+
+  await t
+    .typeText(input, 'My first todo')
+    .click(button)
+    .expect(todoList.withText('My first todo').exists).ok();
 });
